85097a4c39c80103395ac1dccd08c0cc6b2922de
eothein <jens.buysse@gmail.com>
2018-08-28 17:27:10 +0200

    Added exercises for SOLID with code and images

---
+++ srccode/kotlin/Rectangle.kt
@@ -0,0 +1,13 @@
+package o
+
+/**
+ * This class represents a rectangle.
+ * Getters and setters have no needed setters and getters
+ * To use a property, we simply refer to it by name, as if it were a field in Java.
+ */
+class Rectangle(width: Int, height: Int){
+
+    val width = width
+    val height = height
+
+}
