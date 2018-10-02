7b41c6028517d10c253852fe2796061a91958e98
Harm De Weirdt <harm.deweirdt@gmail.com>
2018-10-02 13:12:19 +0200

    Made Manager and Worker classes more idiomatic Kotlin

--- srccode/kotlin/Manager.kt
+++ srccode/kotlin/Manager.kt
@@ -3,13 +3,7 @@ package d
 /**
  * Manager class. Manages only 1 worker for simplicity reasons.
  */
-class Manager(worker : Worker) {
-
-    /**
-     * We create a property which holds the worker
-     * the manager manages.
-     */
-    var worker :Worker
+class Manager(var worker : Worker) {
 
     init {
         this.worker = worker
